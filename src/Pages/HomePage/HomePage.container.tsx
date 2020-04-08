c9181933331627a8fcb04d3249d4459f83680322
PiterM <ptr.kiewicz@gmail.com>
2020-04-08 21:16:52 +0200

    loading and error screens

--- src/Pages/HomePage/HomePage.container.tsx
+++ src/Pages/HomePage/HomePage.container.tsx
@@ -1,19 +1,52 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
+import styled from '@emotion/styled'
 import { GET_SITE_GLOBAL_DATA_QUERY, GET_ORTALIO_MEDIA_QUERY } from './HomePage.gql';
 import HomePage from './HomePage';
 import { GlobalData, OrtalioMedia } from './HomePage.models';
+
+const LoaderScreen = styled.div`
+    background: url('/images/audio-loader.svg') center center no-repeat #fff;
+    fill: #E3C07D;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+`;
+
+const ErrorScreen = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ErrorMessage = styled.div`
+    background: url('/images/error-icon.svg') top center no-repeat #fff;
+    width: 200px;;
+    height: 100px;
+    & > p {
+        margin-top: 85px;
+    }
+`;
 
 const HomePageContainer = () => {
     const globalData = useQuery(GET_SITE_GLOBAL_DATA_QUERY);
     const ortalioMediaData = useQuery(GET_ORTALIO_MEDIA_QUERY);
 
     if (globalData.loading || ortalioMediaData.loading) {
-        return <p>Still loading</p>;
+        return <LoaderScreen />;
     }
 
     if (globalData.error || ortalioMediaData.error) {
-        return <p>Był error</p>;
+        return ( 
+            <ErrorScreen>
+                <ErrorMessage>
+                    <p>Sorry. Come back later.</p>
+                </ErrorMessage>
+            </ErrorScreen>
+        );
     }
 
     return (
