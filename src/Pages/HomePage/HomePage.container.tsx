1f02c303c9f49f7acf84f809819d4cf68bd70758
piter <ptr.kiewicz@gmail.com>
2020-04-27 21:06:50 +0200

    queries component to component

--- src/Pages/HomePage/HomePage.container.tsx
+++ src/Pages/HomePage/HomePage.container.tsx
@@ -18,7 +18,7 @@ const LoaderScreen = styled.div`
 `;
 
 const renderLoadingOrErrorScreen = (data: any) => {
-    if (data.loading) {
+    if (!data || data.loading) {
         return <LoaderScreen />;
     }
     if (data.error) {
@@ -27,51 +27,57 @@ const renderLoadingOrErrorScreen = (data: any) => {
     return null;
 }
 
-const HomePageContainer = React.memo(() => {
-    return (
-        <>
-            <Query query={GQL_QUERIES.GET_SITE_GLOBAL_DATA_QUERY}>
-                {
-                    (globalData: any) => {
-                        const LoadingOrErrorScreen: any = renderLoadingOrErrorScreen(globalData);
-                        if (LoadingOrErrorScreen !== null) {
-                            return LoadingOrErrorScreen;
-                        }
-
-                        return <Query query={GQL_QUERIES.GET_SOCIAL_MEDIA_DATA_QUERY}>
-                            {
-                                (socialMediaData: any) => {
-                                    const LoadingOrErrorScreen: any = renderLoadingOrErrorScreen(socialMediaData);
-                                    if (LoadingOrErrorScreen !== null) {
-                                        return LoadingOrErrorScreen;
-                                    }
-            
-                                    return <Query query={GQL_QUERIES.GET_ORTALIO_MEDIA_QUERY}>
-                                        {
-                                            (ortalioMediaData: any) => {
-                                                const LoadingOrErrorScreen: any = renderLoadingOrErrorScreen(ortalioMediaData);
-                                                if (LoadingOrErrorScreen !== null) {
-                                                    return LoadingOrErrorScreen;
-                                                }
-
-                                                return (
-                                                    <HomePage 
-                                                        globalData={globalData.data.globalData as GlobalData}
-                                                        socialMediaData={socialMediaData.data.socialMediaData as SocialMediaData[]}
-                                                        data={ortalioMediaData.data.data as OrtalioMedia[]}
-                                                    />
-                                                );
-                                            }
-                                        }
-                                    </Query>
-                                }
+class HomePageContainer extends React.Component {
+    shouldComponentUpdate() {
+        return false;
+    }
+    
+    render() {
+        return (
+            <>
+                <Query query={GQL_QUERIES.GET_SITE_GLOBAL_DATA_QUERY}>
+                    {
+                        (globalData: any) => {
+                            const LoadingOrErrorScreen: any = renderLoadingOrErrorScreen(globalData);
+                            if (LoadingOrErrorScreen !== null) {
+                                return LoadingOrErrorScreen;
                             }
-                        </Query>
+                            console.log('ile razy');
+
+                            return <Query query={GQL_QUERIES.GET_SOCIAL_MEDIA_DATA_QUERY}>
+                                {
+                                    (socialMediaData: any) => {
+                                        const LoadingOrErrorScreen: any = renderLoadingOrErrorScreen(socialMediaData);
+                                        if (LoadingOrErrorScreen !== null) {
+                                            return LoadingOrErrorScreen;
+                                        }
+                
+                                        return <Query query={GQL_QUERIES.GET_ORTALIO_MEDIA_QUERY}>
+                                            {
+                                                (ortalioMediaData: any) => {
+                                                    const LoadingOrErrorScreen: any = renderLoadingOrErrorScreen(ortalioMediaData);
+                                                    if (LoadingOrErrorScreen !== null) {
+                                                        return LoadingOrErrorScreen;
+                                                    }
+                                                    return (
+                                                        <HomePage 
+                                                            globalData={globalData.data.globalData as GlobalData}
+                                                            socialMediaData={socialMediaData.data.socialMediaData as SocialMediaData[]}
+                                                            data={ortalioMediaData.data.data as OrtalioMedia[]}
+                                                        />
+                                                    );
+                                                }
+                                            }
+                                        </Query>
+                                    }
+                                }
+                            </Query>
+                        }
                     }
-                }
-            </Query>
-        </>
-    );
-});
+                </Query>
+            </>
+        );
+    }
+}
 
 export default HomePageContainer;
